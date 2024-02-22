5da4979543607e4d9f6a614e106295f073fe2424
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-22 22:10:10 +0100

    Talki 1.0

--- install.js
+++ install.js
@@ -1,34 +1,35 @@
 const { exec } = require('child_process');
 const readline = require('readline');
 const fs = require('fs');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+rl.question('Ingresa el token de acceso a Telegram: ', (telegramToken) => {
+    rl.question('Ingresa el lenguaje del bot: ', (botLanguage) => {
+        rl.question('Ingresa la URL de la base de datos: ', (databaseUrl) => {
+            rl.close();
+            process.env.token = telegramToken;
+            process.env.language = botLanguage;
+            process.env.mongodb = databaseUrl;
+            const config = {
+                token: telegramToken,
+                language: botLanguage,
+                mongodb: databaseUrl
+            };
+            fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
+            console.log('Variables de entorno configuradas:');
+            console.log(`process.env.token: ${process.env.token}`);
+            console.log(`process.env.language: ${process.env.language}`);
+            console.log(`process.env.mongodb: ${process.env.mongodb}`);
             exec('npm install', (error, stdout, stderr) => {
                 if (error) {
                     console.error(`Error al instalar dependencias: ${error}`);
                     return;
                 }
-                rl.question('Ingresa el token de acceso a Telegram: ', (telegramToken) => {
-                    rl.question('Ingresa el lenguaje del bot: ', (botLanguage) => {
-                        rl.question('Ingresa la URL de la base de datos: ', (databaseUrl) => {
-                            rl.close();
-                            process.env.token = telegramToken;
-                            process.env.language = botLanguage;
-                            process.env.mongodb = databaseUrl;
-                            const config = {
-                                token: telegramToken,
-                                language: botLanguage,
-                                mongodb: databaseUrl
-                            };
-                            fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
-                            console.log('Variables de entorno configuradas:');
-                            console.log(`process.env.token: ${process.env.token}`);
-                            console.log(`process.env.language: ${process.env.language}`);
-                            console.log(`process.env.mongodb: ${process.env.mongodb}`);
                 console.log(`Dependencias instaladas exitosamente: ${stdout}`);
-                exec('npm start', (error, stdout, stderr) => {
+                const startProcess = exec('npm start', (error, stdout, stderr) => {
                     if (error) {
                         console.error(`Error al ejecutar npm start: ${error}`);
                         return;
@@ -36,8 +37,9 @@ const rl = readline.createInterface({
                     console.log(`npm start ejecutado exitosamente: ${stdout}`);
                     console.log('El paquete se ha instalado correctamente.');
                 });
+                startProcess.stdout.pipe(process.stdout);
+                startProcess.stderr.pipe(process.stderr);
             });
         });
     });
 });
-
