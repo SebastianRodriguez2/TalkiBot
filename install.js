003461c16c8f42d38db22233d1f9ed87bbdda552
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-22 21:55:31 +0100

    Talki 1.0

--- install.js
+++ install.js
@@ -1,7 +1,6 @@
 const { exec } = require('child_process');
 const readline = require('readline');
 const fs = require('fs');
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -10,22 +9,24 @@ rl.question('Ingresa el token de acceso a Telegram: ', (telegramToken) => {
     rl.question('Ingresa el lenguaje del bot: ', (botLanguage) => {
         rl.question('Ingresa la URL de la base de datos: ', (databaseUrl) => {
             rl.close();
+            process.env.token = telegramToken;
+            process.env.language = botLanguage;
+            process.env.mongodb = databaseUrl;
             const config = {
                 token: telegramToken,
                 language: botLanguage,
                 mongodb: databaseUrl
             };
-
+            fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
             console.log('Variables de entorno configuradas:');
             console.log(`process.env.token: ${process.env.token}`);
             console.log(`process.env.language: ${process.env.language}`);
             console.log(`process.env.mongodb: ${process.env.mongodb}`);
-        
-            fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
-            process.env.token = telegramToken;
-            process.env.language = botLanguage;
-            process.env.mongodb = databaseUrl;
-
+            exec('npm install', (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Error al instalar dependencias: ${error}`);
+                    return;
+                }
                 console.log(`Dependencias instaladas exitosamente: ${stdout}`);
                 exec('npm start', (error, stdout, stderr) => {
                     if (error) {
@@ -38,4 +39,5 @@ rl.question('Ingresa el token de acceso a Telegram: ', (telegramToken) => {
             });
         });
     });
+});
 
