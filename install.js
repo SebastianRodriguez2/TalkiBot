c2dc2f98816f5e21ffd3df6924872ff0f824bd28
Sebastian <120403601+BOT-TX@users.noreply.github.com>
2024-02-22 21:24:22 +0100

    Talki 1.0

--- install.js
+++ install.js
@@ -6,9 +6,9 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-rl.question('\x1b[42m\x1b[30m%s\x1b[0m', 'Ingresa el token de acceso a Telegram: ', (telegramToken) => {
-    rl.question('\x1b[42m\x1b[30m%s\x1b[0m', 'Ingresa el lenguaje del bot: ', (botLanguage) => {
-        rl.question('\x1b[42m\x1b[30m%s\x1b[0m', 'Ingresa la URL de la base de datos: ', (databaseUrl) => {
+rl.question('Ingresa el token de acceso a Telegram: ', (telegramToken) => {
+    rl.question('Ingresa el lenguaje del bot: ', (botLanguage) => {
+        rl.question('Ingresa la URL de la base de datos: ', (databaseUrl) => {
             rl.close();
             const config = {
                 token: telegramToken,
@@ -21,17 +21,17 @@ rl.question('\x1b[42m\x1b[30m%s\x1b[0m', 'Ingresa el token de acceso a Telegram:
             process.env.mongodb = databaseUrl;
             exec('npm install', (error, stdout, stderr) => {
                 if (error) {
-                    console.error('\x1b[41m\x1b[30m%s\x1b[0m', `Error al instalar dependencias: ${error}`);
+                    console.error(`Error al instalar dependencias: ${error}`);
                     return;
                 }
-                console.log('\x1b[42m\x1b[30m%s\x1b[0m', `Dependencias instaladas exitosamente: ${stdout}`);
+                console.log(`Dependencias instaladas exitosamente: ${stdout}`);
                 exec('npm start', (error, stdout, stderr) => {
                     if (error) {
-                        console.error('\x1b[41m\x1b[30m%s\x1b[0m', `Error al ejecutar npm start: ${error}`);
+                        console.error(`Error al ejecutar npm start: ${error}`);
                         return;
                     }
-                    console.log('\x1b[42m\x1b[30m%s\x1b[0m', `npm start ejecutado exitosamente: ${stdout}`);
-                    console.log('\x1b[42m\x1b[30m%s\x1b[0m', 'El paquete se ha instalado correctamente.');
+                    console.log(`npm start ejecutado exitosamente: ${stdout}`);
+                    console.log('El paquete se ha instalado correctamente.');
                 });
             });
         });
